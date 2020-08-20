260afe6724b80e7f8dbc579e4b3ef6bdcd59b965
xuguo <xuguo@leimingtech.com>
2020-08-20 14:40:39 +0800

    build: 添加rollup打包

---
+++ rollup.config.js
@@ -0,0 +1,107 @@
+import path from 'path'
+import ts from 'rollup-plugin-typescript2'
+import dts from 'rollup-plugin-dts'
+
+const resolve = (p) => path.resolve(__dirname, p)
+let formats = process.env.FORMATS
+const needBuildTypes = !!process.env.BUILDTYPES
+const pkgJson = require(resolve('./package.json'))
+
+const outputConfigMap = {
+  esm: {
+    file: resolve(`dist/vue.hooks.esm.js`),
+    format: 'es',
+  },
+  cjs: {
+    file: resolve(`dist/vue.hooks.cjs.js`),
+    format: 'cjs',
+  },
+  umd: {
+    file: resolve(`dist/vue.hooks.js`),
+    format: 'umd',
+    name: 'VueHooks'
+  },
+}
+
+let RollupConfigs = []
+
+formats = formats ? formats.split('-') : Object.keys(outputConfigMap)
+
+formats.forEach(format => {
+  RollupConfigs.push(createConfig(format, outputConfigMap[format]))
+})
+
+needBuildTypes &&
+  RollupConfigs.push({
+    input: resolve(`dist/src/index.d.ts`),
+    output: {
+      file: resolve(`dist/vue.hooks.d.ts`),
+      format: 'es',
+    },
+    plugins: [dts()],
+  })
+
+export default RollupConfigs
+
+function createConfig(format, output) {
+  if (format === 'umd') {
+    output.sourcemap = !!process.env.SOURCEMAP
+  }
+  // 打包ts
+  const tsPlugin = ts({
+    check: process.env.NODE_ENV === 'production',
+    tsconfigDefaults: resolve('tsconfig.json'),
+    cacheRoot: resolve('./node_modules/.ts2_cache'),
+    tsconfigOverride: {
+      compilerOptions: {
+        target: 'es5',
+        sourceMap: !!process.env.SOURCEMAP,
+        removeComments: true,
+        declaration: needBuildTypes,
+        declarationMap: needBuildTypes,
+      },
+      include: [`src/`],
+      exclude: ['__test__/**'],
+    },
+  })
+  // external
+  const external = [
+    ...Object.keys(pkgJson.peerDependencies || {}),
+    ...Object.keys(pkgJson.dependencies || {}),
+    ...(pkgJson.external || [])
+  ]
+  // node plugins
+  const nodePlugins =
+  format === 'cjs'
+      ? []
+      : [
+        require('@rollup/plugin-node-resolve').nodeResolve({
+          referBuiltins: true,
+        }),
+        require('@rollup/plugin-commonjs')({
+          sourceMap: true,
+        }),
+      ]
+  // prod plugins
+  const prodPlugins = 
+  process.env.NODE_ENV === 'production'
+  ? [
+    require('rollup-plugin-terser').terser({
+      module: format === 'esm',
+      compress: {
+        ecma: 2015,
+      },
+    })
+  ]
+  : []
+
+  return {
+    input: resolve('src/index.ts'),
+    output,
+    plugins: [tsPlugin, ...nodePlugins, ...prodPlugins],
+    external,
+    treeshake: {
+      moduleSideEffects: false,
+    },
+  }
+}
---
+++ scripts/build.js
@@ -0,0 +1,43 @@
+const path = require('path')
+const fs = require('fs-extra')
+const execa = require('execa')
+const chalk = require('chalk')
+// 参数处理
+const resolve = p => path.resolve(__dirname, '../', p)
+const args = require('minimist')(process.argv.slice(2))
+const formats = args._
+const prod = args.prod || args.p
+const sourceMap = args.sourceMap || args.s
+const buildTypes = args.types || args.t
+
+async function run() {
+  // 如果指定了格式 则清除原dist文件夹
+  if (!formats) {
+    await fs.remove(resolve('dist'))
+  }
+  // 环境信息
+  const env = prod ? 'production' : 'development'
+  console.info(chalk.bold(chalk.yellow(`Rollup for vue-hooks`)))
+  await execa(
+    'rollup',
+    [
+      '-c',
+      '--environment',
+      [
+        `NODE_ENV:${env}`,
+        formats ? `FORMATS:${formats.join('-')}` : '',
+        sourceMap ? `SOURCEMAP:true` : '',
+        buildTypes ? `BUILDTYPES:true` : '',
+      ]
+        .filter(Boolean)
+        .join(','),
+    ],
+    {
+      stdio: 'inherit',
+    }
+  )
+  
+  fs.remove(resolve(`dist/src`))
+}
+
+run()
