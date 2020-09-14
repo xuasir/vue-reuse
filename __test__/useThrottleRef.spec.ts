70cccf74481f3797ddd9aeda791c8fea08bd6ac1
xuguo <xuguo@leimingtech.com>
2020-09-14 11:56:07 +0800

    test(sideeffect): 更新debounceRef和throttleRef测试用例

--- __test__/useThrottleRef.spec.ts
+++ __test__/useThrottleRef.spec.ts
@@ -1,4 +1,3 @@
-import { ref } from 'vue-demi'
 import { useThrottleRef } from '../src'
 
 type noop = (...args: any[]) => any
@@ -7,13 +6,12 @@ describe('test useThrottleRef', () => {
   const nextTask = (fn: noop, wait: number) => setTimeout(fn, wait)
 
   test('should be run first time when run', () => {
-    const rawValue = ref<number>(0)
-    const throttleValue = useThrottleRef(rawValue, 500)
-    rawValue.value = 1
-    rawValue.value = 2
-    rawValue.value = 3
+    const throttleValue = useThrottleRef(0, 500)
+    throttleValue.value = 1
+    throttleValue.value = 2
+    throttleValue.value = 3
+    expect(throttleValue.value).toBe(1)
     nextTask(() => {
-      expect(rawValue.value).toBe(3)
       expect(throttleValue.value).toBe(1)
     }, 550)
   })
