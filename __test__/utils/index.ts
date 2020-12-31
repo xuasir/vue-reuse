860257c24bcae66f204e16c689704fcda0b351b9
guo.xu <xuguo@outlook.it>
2020-12-31 08:10:50 +0800

    test(useclickoutside): 为clickoutside添加单元测试

---
+++ __test__/utils/index.ts
@@ -0,0 +1,25 @@
+import { mount, Wrapper } from '@vue/test-utils'
+import { noop } from './types'
+
+export function renderComposable(
+  cb: () => any,
+  attachTo?: Element | string
+): Wrapper<Vue> {
+  return mount(
+    {
+      setup() {
+        return cb()
+      },
+      render(h) {
+        return h('div')
+      }
+    },
+    { attachTo }
+  )
+}
+
+export function nextTask(fn: noop): void {
+  setTimeout(fn)
+}
+
+export * from './types'
---
+++ __test__/utils/types.ts
@@ -0,0 +1,1 @@
+export type noop = (...args: any[]) => any
