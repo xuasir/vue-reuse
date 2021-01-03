b0520f7712dfd5a2b1dfe4ca4d2bc1c1be95997e
wings <314912145@qq.com>
2021-01-03 22:52:18 +0800

    feat(src/useBoolean): 新增boolean功能集合

---
+++ __test__/useBoolean.spec.ts
@@ -0,0 +1,58 @@
+import { useBoolean } from '../src'
+import { reactive } from 'vue-demi'
+
+describe('test useBoolean', () => {
+  test('test state and actions', async () => {
+    const [state, { changeState, setTrue, setFalse }] = useBoolean(true)
+    expect(state.value).toBe(true)
+
+    setFalse()
+    expect(state.value).toBe(false)
+
+    setTrue()
+
+    expect(state.value).toBe(true)
+
+    changeState()
+
+    expect(state.value).toBe(!state)
+  })
+
+  test('test state and actions', async () => {
+    let options: {
+      [propName: string]: boolean
+    } = reactive({
+      0: true,
+      '': true,
+      usefalse: false
+    })
+
+    const [state, { changeState }] = useBoolean(true, options)
+
+    expect(state.value).toBe(true)
+
+    changeState(0)
+
+    expect(state.value).toBe(true)
+
+    changeState('usefalse')
+
+    expect(state.value).toBe(false)
+
+    changeState('')
+
+    expect(state.value).toBe(true)
+
+    options.usefalse = true
+
+    changeState('usefalse')
+
+    expect(state.value).toBe(true)
+
+    options.addOptions = false
+
+    changeState('addOptions')
+
+    expect(state.value).toBe(false)
+  })
+})
