ec6a7853d55ac63dbcf70635320fc290bd0a20cf
guo.xu <xuguo@outlook.it>
2021-01-08 11:05:33 +0800

    test(requestcache): 添加request-cache相关功能的测试

--- __test__/useRequest.spec.ts
+++ __test__/useRequest.spec.ts
@@ -1,5 +1,6 @@
 import { Wrapper } from '@vue/test-utils'
-import { useAsync } from '../src/useRequest/useAsync'
+import { useRequest } from '../src/useRequest'
+import { getCache } from '../src/useRequest/utils/cache'
 import { waitTime, renderComposable } from './utils'
 
 const Fetch = (p: number) =>
@@ -23,15 +24,29 @@ const parallelFetch = (p: { key: string; res: number }) =>
     }, 100)
   })
 
+const customRequest = (p: string) =>
+  new Promise<string>((resolve) => {
+    setTimeout(() => {
+      resolve(p)
+    }, 100)
+  })
+
+const customRequest2 = (p: string, a: string, b: string) =>
+  new Promise<string>((resolve) => {
+    setTimeout(() => {
+      resolve(p + a + b)
+    }, 100)
+  })
+
 describe('test use async ', () => {
   let wrapper: Wrapper<Vue>
   afterEach(() => {
     wrapper.destroy()
   })
 
-  test('test async ', async () => {
+  test('test request ', async () => {
     wrapper = renderComposable(() =>
-      useAsync(Fetch, {
+      useRequest(Fetch, {
         defaultParams: [1]
       })
     )
@@ -52,9 +67,9 @@ describe('test use async ', () => {
     expect(vm.$data.data).toEqual(1)
   })
 
-  test(`test async manaul `, async () => {
+  test(`test request manaul `, async () => {
     wrapper = renderComposable(() =>
-      useAsync(Fetch, {
+      useRequest(Fetch, {
         defaultData: 1,
         manual: true
       })
@@ -76,9 +91,9 @@ describe('test use async ', () => {
     expect(vm.$data.data).toBe(3)
   })
 
-  test(`test async parallel `, async () => {
+  test(`test request parallel `, async () => {
     wrapper = renderComposable(() =>
-      useAsync(parallelFetch, {
+      useRequest(parallelFetch, {
         manual: true,
         fetchKey: (p) => p.key
       })
@@ -115,9 +130,9 @@ describe('test use async ', () => {
     expect(vm.$data.fetches['2'].data).toBe(20)
   })
 
-  test(`test async formatResult `, async () => {
+  test(`test request formatResult `, async () => {
     wrapper = renderComposable(() =>
-      useAsync(Fetch, {
+      useRequest(Fetch, {
         defaultParams: [1],
         formatResult: (res) => ({ res }),
         defaultData: { res: 2 }
@@ -135,9 +150,9 @@ describe('test use async ', () => {
     expect(vm.$data.data).toEqual({ res: 1 })
   })
 
-  test(`test async debouce `, async () => {
+  test(`test request debouce `, async () => {
     wrapper = renderComposable(() =>
-      useAsync(Fetch, {
+      useRequest(Fetch, {
         manual: true,
         debounceTime: 50
       })
@@ -152,9 +167,9 @@ describe('test use async ', () => {
     expect(vm.$data.data).toBe(4)
   })
 
-  test(`test async throttle `, async () => {
+  test(`test request throttle `, async () => {
     wrapper = renderComposable(() =>
-      useAsync(Fetch, {
+      useRequest(Fetch, {
         manual: true,
         throttleTime: 50
       })
@@ -169,10 +184,10 @@ describe('test use async ', () => {
     expect(vm.$data.data).toBe(4)
   })
 
-  test(`test async polling `, async () => {
+  test(`test request polling `, async () => {
     const onSuccess = jest.fn()
     wrapper = renderComposable(() =>
-      useAsync(Fetch, {
+      useRequest(Fetch, {
         manual: true,
         pollingTime: 50,
         onSuccess
@@ -192,7 +207,7 @@ describe('test use async ', () => {
 
   test(`test loading delay `, async () => {
     wrapper = renderComposable(() =>
-      useAsync(Fetch, {
+      useRequest(Fetch, {
         loadingDelay: 20,
         defaultParams: [1]
       })
@@ -213,10 +228,10 @@ describe('test use async ', () => {
     expect(vm.$data.data).toBe(1)
   })
 
-  test(`test async error `, async () => {
+  test(`test request error `, async () => {
     const onError = jest.fn()
     wrapper = renderComposable(() =>
-      useAsync(FetchError, {
+      useRequest(FetchError, {
         manual: true,
         onError
       })
@@ -235,10 +250,10 @@ describe('test use async ', () => {
     expect(vm.$data.error).toBe(1)
   })
 
-  test(`test async error throw `, async () => {
+  test(`test request error throw `, async () => {
     const onError = jest.fn()
     wrapper = renderComposable(() =>
-      useAsync(FetchError, {
+      useRequest(FetchError, {
         manual: true,
         onError,
         throwOnError: true
@@ -254,5 +269,171 @@ describe('test use async ', () => {
     expect(onError).toBeCalled()
     expect(onError).toBeCalledWith(2, [2])
     expect(vm.$data.error).toBe(2)
+  })
+
+  test(`test request reset `, async () => {
+    wrapper = renderComposable(() =>
+      useRequest(Fetch, {
+        manual: true
+      })
+    )
+    const { vm } = wrapper
+    await vm.$nextTick()
+    expect(vm.$data.data).toBeUndefined()
+    await vm.$data.run(2)
+    expect(vm.$data.data).toBe(2)
+    vm.$data.reset()
+    await vm.$nextTick()
+    expect(vm.$data.data).toBe(2)
+    expect(vm.$data.fetches).toEqual({})
+  })
+
+  test(`test request cache `, async () => {
+    const CacheKey = 'test_cache'
+    const DefaultKey = `vue_reuse_request_default_fetch_key`
+    wrapper = renderComposable(() =>
+      useRequest(Fetch, {
+        defaultParams: [1],
+        cacheKey: CacheKey,
+        cacheTime: 200
+      })
+    )
+    const { vm } = wrapper
+    // after mount
+    await vm.$nextTick()
+    expect(vm.$data.loading).toBeTruthy()
+    expect(vm.$data.params).toEqual([1])
+    expect(vm.$data.data).toBeUndefined()
+    await waitTime(110)
+    expect(vm.$data.loading).toBeFalsy()
+    expect(vm.$data.params).toEqual([1])
+    expect(vm.$data.data).toBe(1)
+    vm.$destroy()
+    const cached = getCache(CacheKey)
+    expect(cached.data?.fetchKey).toBe(DefaultKey)
+    expect(cached.data?.fetches[DefaultKey].data).toBe(1)
+    // in any where request again cache data will be initial
+    wrapper = renderComposable(() =>
+      useRequest(Fetch, {
+        defaultParams: [2],
+        cacheKey: CacheKey,
+        cacheTime: 200
+      })
+    )
+    const { vm: vm2 } = wrapper
+    // after mount
+    await vm2.$nextTick()
+    expect(vm2.$data.loading).toBeTruthy()
+    expect(vm2.$data.params).toEqual([2])
+    expect(vm2.$data.data).toBe(1)
+    // after request
+    await waitTime(110)
+    expect(vm2.$data.loading).toBeFalsy()
+    expect(vm2.$data.params).toEqual([2])
+    expect(vm2.$data.data).toBe(2)
+    // after clear cache
+    await waitTime(100)
+    const cached2 = getCache(CacheKey)
+    expect(cached2.data?.fetchKey).toBeUndefined()
+    expect(cached2.data?.fetches).toBeUndefined()
+  })
+
+  test(`test custom request method `, async () => {
+    wrapper = renderComposable(() =>
+      useRequest('1', {
+        requestMethod: customRequest2,
+        defaultParams: ['2', '3']
+      })
+    )
+    const { vm } = wrapper
+    await vm.$nextTick()
+    expect(vm.$data.loading).toBe(true)
+    expect(vm.$data.params).toEqual(['2', '3'])
+    expect(vm.$data.data).toBeUndefined()
+    await waitTime(110)
+    expect(vm.$data.loading).toBe(false)
+    expect(vm.$data.params).toEqual(['2', '3'])
+    expect(vm.$data.data).toBe('123')
+  })
+
+  test(`test custom request method functional service `, async () => {
+    const url = (p: string) => p
+    wrapper = renderComposable(() =>
+      useRequest(url, {
+        requestMethod: customRequest,
+        defaultParams: ['1']
+      })
+    )
+    const { vm } = wrapper
+    await vm.$nextTick()
+    expect(vm.$data.loading).toBe(true)
+    expect(vm.$data.params).toEqual(['1'])
+    expect(vm.$data.data).toBeUndefined()
+    await waitTime(110)
+    expect(vm.$data.loading).toBe(false)
+    expect(vm.$data.params).toEqual(['1'])
+    expect(vm.$data.data).toBe('1')
+  })
+
+  test(`test custom request method object service `, async () => {
+    const customRequest = ({ url }: { url: string }) =>
+      new Promise<string>((resolve) => {
+        setTimeout(() => {
+          resolve(url)
+        }, 100)
+      })
+    const url = (url: string) => ({ url })
+    wrapper = renderComposable(() =>
+      useRequest(url, {
+        requestMethod: customRequest,
+        defaultParams: ['1']
+      })
+    )
+    const { vm } = wrapper
+    await vm.$nextTick()
+    expect(vm.$data.loading).toBe(true)
+    expect(vm.$data.params).toEqual(['1'])
+    expect(vm.$data.data).toBeUndefined()
+    await waitTime(110)
+    expect(vm.$data.loading).toBe(false)
+    expect(vm.$data.params).toEqual(['1'])
+    expect(vm.$data.data).toBe('1')
+    vm.$destroy()
+    // object
+    wrapper = renderComposable(() =>
+      useRequest(
+        { url: '1' },
+        {
+          requestMethod: customRequest
+        }
+      )
+    )
+    const { vm: vm1 } = wrapper
+    await vm1.$nextTick()
+    expect(vm1.$data.loading).toBe(true)
+    expect(vm1.$data.params).toEqual([])
+    expect(vm1.$data.data).toBeUndefined()
+    await waitTime(110)
+    expect(vm1.$data.loading).toBe(false)
+    expect(vm1.$data.params).toEqual([])
+    expect(vm1.$data.data).toBe('1')
+  })
+
+  test(`test global fetch `, async () => {
+    wrapper = renderComposable(() =>
+      useRequest('1', {
+        manual: true
+      })
+    )
+    const { vm } = wrapper
+    await vm.$nextTick()
+    expect(vm.$data.data).toBeUndefined()
+    await vm.$data.run()
+    expect(vm.$data.data).toBe('1')
+    await vm.$data.run({ error: 'error' }).catch((err: Error) => {
+      expect(err).toBe(
+        `useRequest has caught the exception, if you need to handle the exception yourself, you can set options.throwOnError to true.`
+      )
+    })
   })
 })
